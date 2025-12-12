61949adc18c6ca0cde2ba77091e3b4e3b1c55ec5
JoaoV4C <jvalves62@outlook.com>
2025-12-12 02:48:54 -0300

    feat: tutorial screen

--- scripts/tilescreen.gd
+++ scripts/tilescreen.gd
@@ -10,6 +10,8 @@ func _process(delta):
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/intro.tscn")
 
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/tutorial.tscn")
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
