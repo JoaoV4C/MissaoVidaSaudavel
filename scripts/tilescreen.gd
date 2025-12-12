36c5e9ca68bbea18f61a9db62eb2486aa64d42b6
JoaoV4C <jvalves62@outlook.com>
2025-12-12 15:00:32 -0300

    feat: credits

--- scripts/tilescreen.gd
+++ scripts/tilescreen.gd
@@ -13,5 +13,8 @@ func _on_button_pressed() -> void:
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/tutorial.tscn")
 
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/credits.tscn")
+
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
